e7fe61feef4869dae6edf4bbd63c2954e97cf596
John <john.grindall@gmail.com>
2018-05-27 12:45:19 +0100

    config wrong

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: 'John Grindall',
     author: 'John Grindall',
     description: 'John Grindall.',
-    siteUrl: 'http://johngrindall.github.io/',
+    siteUrl: 'http://jgrindall.github.io/',
   },
   pathPrefix: '/',
   plugins: [
@@ -38,12 +38,6 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
-      },
-    },
     `gatsby-plugin-feed`,
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`
